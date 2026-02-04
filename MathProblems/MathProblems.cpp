3eb6906637cecf6439f515d49632564f2066c446
Loris M. <80474693+Loris-Moreau@users.noreply.github.com>
2026-02-04 13:57:08 +0100

    Update MathProblems.cpp

--- MathProblems/MathProblems.cpp
+++ MathProblems/MathProblems.cpp
@@ -11,11 +11,39 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    //TSP();
-    //CollatzConjecture();
-    //Fibonacci(25);
-    //Meissel_Lehmer();
-    //PollardRho();
+    int i = 1, c = 25;
+
+    cout << "Available Functions : " << '\n' 
+        << "1: TSP" << '\n' 
+        << "2: Collatz Conjecture" << '\n' 
+        << "3: Fibonacci Sequence" << '\n' 
+        << "3: Meissel Lehmer Algorithm" << '\n' 
+        << "5: Pollard's Rho" << '\n' ;
+    cout << "Choose : ";
+    cin >> i;
+    
+    switch (i)
+    {
+    case 1:
+        TSP();
+        return 0;
+    case 2:
+        CollatzConjecture();
+        return 0;
+    case 3:
+        cout << "Amount of conjecture : ";
+        cin >> c;
+        Fibonacci(c, false); 
+        return 0;
+    case 4:
+        Meissel_Lehmer();
+        return 0;
+    case 5:
+        PollardRho();
+        return 0;
+    default:
+        return 0;
+    }
     
     return 0;
 }
